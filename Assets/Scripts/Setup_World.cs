19eb254ad4de444c7a27a4d2873827b7cabe8f6a
Vladimir <fedorenkokhalilov@gmail.com>
2017-10-28 10:30:24 +0300

    unit-explode-as-voxels
    unit-explode-as-voxels

---
+++ Assets/Scripts/Setup_World.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setup_World : MonoBehaviour {
+
+	[SerializeField]
+	private GUIStyle guiStyle;
+
+	GameObject instance;
+	GameObject voxel;
+	ParticleSystem particle;
+	SkinnedMeshRenderer rend;
+
+	void Start()
+	{
+		// Instantiates a prefab named "enemy" located in any Resources
+		// folder in project's Assets folder.
+		instance = Instantiate(Resources.Load("FlyUnit_lowPoly", typeof(GameObject))) as GameObject;
+		instance.transform.position = new Vector3 (0, -5);
+		instance.transform.localScale = new Vector3 (3, 3, 3);
+
+		Animator anim = instance.GetComponent<Animator> ();
+		anim.runtimeAnimatorController = Resources.Load ("FlyUnit_ac") as RuntimeAnimatorController;
+
+		rend = instance.GetComponentInChildren(typeof(SkinnedMeshRenderer)) as SkinnedMeshRenderer;
+		rend.material.mainTexture = Resources.Load("sss") as Texture;
+
+
+
+
+	}
+
+	void LateUpdate()
+	{
+		instance.transform.RotateAround (instance.transform.position, Vector3.up, .5f);
+		if (particle.isStopped && particle != null) {
+			Destroy (GameObject.FindGameObjectWithTag ("ps"));
+		} 
+	}
+
+	void OnGUI() {
+		//GUI.color = Color.red;
+		guiStyle.fontSize = 25; //change the font size
+		if(instance!=null){
+			if (GUI.Button (new Rect (0, 0, Screen.width/2, Screen.height/6), "Destroy", guiStyle)) {
+				this.gameObject.GetComponent<AudioSource> ().Play ();
+				Transform temp = instance.transform;
+			particle = Instantiate(Resources.Load("Particle System", typeof(ParticleSystem))) as ParticleSystem;
+			Destroy (instance);
+					voxel = Instantiate (Resources.Load ("VoxeldFlyUnit", typeof(GameObject))) as GameObject;
+				voxel.transform.position = temp.transform.position;
+		}
+		}
+
+		if(particle!=null){
+			if (GUI.Button (new Rect (0, 0, Screen.width/2, Screen.height/6),  "One more FlyUnit", guiStyle)) {
+				Destroy (voxel);
+				instance = Instantiate(Resources.Load("FlyUnit_lowPoly", typeof(GameObject))) as GameObject;
+				instance.transform.position = new Vector3 (0, -5);
+				instance.transform.localScale = new Vector3 (3, 3, 3);
+				rend = instance.GetComponentInChildren(typeof(SkinnedMeshRenderer)) as SkinnedMeshRenderer;
+				rend.material.mainTexture = Resources.Load("sss") as Texture;
+			}
+		}
+
+		if (GUI.Button (new Rect (Screen.width/2, 1, Screen.width/2, Screen.height/6), "Quit", guiStyle)) {
+			System.Diagnostics.Process.GetCurrentProcess().Kill();
+			Application.Quit ();
+		}
+	}
+}
---
+++ Assets/Scripts/VoxelizeDestroy.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace mattatz.VoxelSystem {
+
+    [RequireComponent (typeof(MeshFilter))]
+    public class VoxelizeDestroy : MonoBehaviour {
+
+		[SerializeField] [Range(5,20)] 
+		int count = 10;
+		[SerializeField] 
+		int blowPower =10;
+		[SerializeField] 
+		int radius = 5;
+		[SerializeField][Range(1,10)] 
+		int massOfParts = 1;
+
+        List<Voxel> voxels;
+
+        void Start () {
+            var filter = GetComponent<MeshFilter>();
+            voxels = Voxelizer.Voxelize(filter.mesh, count);
+			voxels.ForEach (voxel => {
+				var cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+				cube.transform.parent = transform;
+				cube.transform.localPosition = voxel.position;
+				cube.transform.localScale = voxel.size * Vector3.one;
+				cube.transform.localRotation = Quaternion.identity;
+				cube.GetComponent<Renderer>().material = this.GetComponent<Renderer>().material;
+				Vector3 explosionPos = cube.transform.localPosition;
+
+				cube.AddComponent<Rigidbody> ();
+
+			});
+
+			for (int i = 0; i < this.transform.childCount; i++) {
+				Collider[] colliders = Physics.OverlapSphere(this.transform.GetChild(i).localPosition, radius);
+				foreach (Collider hit in colliders) {
+					Rigidbody rb = this.transform.GetChild (i).GetComponent<Rigidbody> ();
+					rb.mass = massOfParts;
+					if (rb != null)
+						rb.AddExplosionForce (blowPower, this.transform.GetChild (i).localPosition, radius, 5.0F);
+				}
+			}
+
+        }
+        
+        // void Update () {}
+
+//        void OnDrawGizmos () {
+//            if (voxels == null) return;
+//
+//            Gizmos.matrix = transform.localToWorldMatrix;
+//            voxels.ForEach(voxel => {
+//				Gizmos.DrawCube(voxel.position, voxel.size * new Vector3(20,20,20));
+//            });
+//        }
+
+    }
+
+}
+
+
